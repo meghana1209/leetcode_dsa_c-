1bea896ee0ddb5bb4b459c3178c6ee84799fc0b9
Meghana B <meghanab235@gmail.com>
2025-02-09 10:36:01 +0530

    day2

--- 169.majority element.cpp
+++ 169.majority element.cpp
@@ -6,7 +6,6 @@ public:
     int majorityElement(vector<int>& nums) {
        int freq=0,ans=0;
       
-
        for(int i=0;i<nums.size();i++){
            if(freq==0){
                  ans=nums[i];
